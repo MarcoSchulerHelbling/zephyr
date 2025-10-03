dd91cf6ac02c2d46c5f99d57c7a54337237d7578
Marco Schuler <marco.schuler@helbling.ch>
2025-10-03 13:03:42 +0200

    DRV: Rename all occurences of DSL/dsl to DSR/dsr in driver/bindings
    Gitlint
    
    Signed-off-by: Marco Schuler <marco.schuler@helbling.ch>

--- drivers/sensor/st/lsm6dsr/lsm6dsl_spi.c
+++ drivers/sensor/st/lsm6dsr/lsm6dsl_spi.c
@@ -1,4 +1,4 @@
-/* lsm6dsl_spi.c - SPI routines for LSM6DSL driver
+/* lsm6dsr_spi.c - SPI routines for LSM6DSR driver
  */
 
 /*
@@ -7,24 +7,24 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#define DT_DRV_COMPAT st_lsm6dsl
+#define DT_DRV_COMPAT st_lsm6dsr
 
 #include <string.h>
 #include <zephyr/logging/log.h>
 
-#include "lsm6dsl.h"
+#include "lsm6dsr.h"
 
 #if DT_ANY_INST_ON_BUS_STATUS_OKAY(spi)
 
-#define LSM6DSL_SPI_READ		(1 << 7)
+#define LSM6DSR_SPI_READ		(1 << 7)
 
-LOG_MODULE_DECLARE(LSM6DSL, CONFIG_SENSOR_LOG_LEVEL);
+LOG_MODULE_DECLARE(LSM6DSR, CONFIG_SENSOR_LOG_LEVEL);
 
-static int lsm6dsl_raw_read(const struct device *dev, uint8_t reg_addr,
+static int lsm6dsr_raw_read(const struct device *dev, uint8_t reg_addr,
 			    uint8_t *value, uint8_t len)
 {
-	const struct lsm6dsl_config *cfg = dev->config;
-	uint8_t buffer_tx[2] = { reg_addr | LSM6DSL_SPI_READ, 0 };
+	const struct lsm6dsr_config *cfg = dev->config;
+	uint8_t buffer_tx[2] = { reg_addr | LSM6DSR_SPI_READ, 0 };
 	const struct spi_buf tx_buf = {
 			.buf = buffer_tx,
 			.len = 2,
@@ -60,11 +60,11 @@ static int lsm6dsl_raw_read(const struct device *dev, uint8_t reg_addr,
 	return 0;
 }
 
-static int lsm6dsl_raw_write(const struct device *dev, uint8_t reg_addr,
+static int lsm6dsr_raw_write(const struct device *dev, uint8_t reg_addr,
 			     uint8_t *value, uint8_t len)
 {
-	const struct lsm6dsl_config *cfg = dev->config;
-	uint8_t buffer_tx[1] = { reg_addr & ~LSM6DSL_SPI_READ };
+	const struct lsm6dsr_config *cfg = dev->config;
+	uint8_t buffer_tx[1] = { reg_addr & ~LSM6DSR_SPI_READ };
 	const struct spi_buf tx_buf[2] = {
 		{
 			.buf = buffer_tx,
@@ -92,48 +92,48 @@ static int lsm6dsl_raw_write(const struct device *dev, uint8_t reg_addr,
 	return 0;
 }
 
-static int lsm6dsl_spi_read_data(const struct device *dev, uint8_t reg_addr,
+static int lsm6dsr_spi_read_data(const struct device *dev, uint8_t reg_addr,
 				 uint8_t *value, uint8_t len)
 {
-	return lsm6dsl_raw_read(dev, reg_addr, value, len);
+	return lsm6dsr_raw_read(dev, reg_addr, value, len);
 }
 
-static int lsm6dsl_spi_write_data(const struct device *dev, uint8_t reg_addr,
+static int lsm6dsr_spi_write_data(const struct device *dev, uint8_t reg_addr,
 				  uint8_t *value, uint8_t len)
 {
-	return lsm6dsl_raw_write(dev, reg_addr, value, len);
+	return lsm6dsr_raw_write(dev, reg_addr, value, len);
 }
 
-static int lsm6dsl_spi_read_reg(const struct device *dev, uint8_t reg_addr,
+static int lsm6dsr_spi_read_reg(const struct device *dev, uint8_t reg_addr,
 				uint8_t *value)
 {
-	return lsm6dsl_raw_read(dev, reg_addr, value, 1);
+	return lsm6dsr_raw_read(dev, reg_addr, value, 1);
 }
 
-static int lsm6dsl_spi_update_reg(const struct device *dev, uint8_t reg_addr,
+static int lsm6dsr_spi_update_reg(const struct device *dev, uint8_t reg_addr,
 				  uint8_t mask, uint8_t value)
 {
 	uint8_t tmp_val;
 
-	lsm6dsl_raw_read(dev, reg_addr, &tmp_val, 1);
+	lsm6dsr_raw_read(dev, reg_addr, &tmp_val, 1);
 	tmp_val = (tmp_val & ~mask) | (value & mask);
 
-	return lsm6dsl_raw_write(dev, reg_addr, &tmp_val, 1);
+	return lsm6dsr_raw_write(dev, reg_addr, &tmp_val, 1);
 }
 
-static const struct lsm6dsl_transfer_function lsm6dsl_spi_transfer_fn = {
-	.read_data = lsm6dsl_spi_read_data,
-	.write_data = lsm6dsl_spi_write_data,
-	.read_reg  = lsm6dsl_spi_read_reg,
-	.update_reg = lsm6dsl_spi_update_reg,
+static const struct lsm6dsr_transfer_function lsm6dsr_spi_transfer_fn = {
+	.read_data = lsm6dsr_spi_read_data,
+	.write_data = lsm6dsr_spi_write_data,
+	.read_reg  = lsm6dsr_spi_read_reg,
+	.update_reg = lsm6dsr_spi_update_reg,
 };
 
-int lsm6dsl_spi_init(const struct device *dev)
+int lsm6dsr_spi_init(const struct device *dev)
 {
-	struct lsm6dsl_data *data = dev->data;
-	const struct lsm6dsl_config *cfg = dev->config;
+	struct lsm6dsr_data *data = dev->data;
+	const struct lsm6dsr_config *cfg = dev->config;
 
-	data->hw_tf = &lsm6dsl_spi_transfer_fn;
+	data->hw_tf = &lsm6dsr_spi_transfer_fn;
 
 	if (!spi_is_ready_dt(&cfg->bus_cfg.spi)) {
 		return -ENODEV;
