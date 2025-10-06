accba76a981192cfffc699d2d873c8c3c6b606e6
Marco Schuler <marco.schuler@helbling.ch>
2025-10-06 13:55:27 +0200

    sample: Delete trigger stuff

--- samples/sensor/lsm6dsr/src/main.c
+++ samples/sensor/lsm6dsr/src/main.c
@@ -11,44 +11,9 @@
 #include <zephyr/sys/util.h>
 
 static int print_samples;
-// static int lsm6dsr_trig_cnt;
 
 static struct sensor_value accel_x_out, accel_y_out, accel_z_out;
 static struct sensor_value gyro_x_out, gyro_y_out, gyro_z_out;
-
-// #ifdef CONFIG_LSM6DSR_TRIGGER
-// static void lsm6dsr_trigger_handler(const struct device *dev,
-// 				    const struct sensor_trigger *trig)
-// {
-// 	static struct sensor_value accel_x, accel_y, accel_z;
-// 	static struct sensor_value gyro_x, gyro_y, gyro_z;
-// 	lsm6dsr_trig_cnt++;
-
-// 	sensor_sample_fetch_chan(dev, SENSOR_CHAN_ACCEL_XYZ);
-// 	sensor_channel_get(dev, SENSOR_CHAN_ACCEL_X, &accel_x);
-// 	sensor_channel_get(dev, SENSOR_CHAN_ACCEL_Y, &accel_y);
-// 	sensor_channel_get(dev, SENSOR_CHAN_ACCEL_Z, &accel_z);
-
-// 	/* lsm6dsr gyro */
-// 	sensor_sample_fetch_chan(dev, SENSOR_CHAN_GYRO_XYZ);
-// 	sensor_channel_get(dev, SENSOR_CHAN_GYRO_X, &gyro_x);
-// 	sensor_channel_get(dev, SENSOR_CHAN_GYRO_Y, &gyro_y);
-// 	sensor_channel_get(dev, SENSOR_CHAN_GYRO_Z, &gyro_z);
-
-// 	if (print_samples) {
-// 		print_samples = 0;
-
-// 		accel_x_out = accel_x;
-// 		accel_y_out = accel_y;
-// 		accel_z_out = accel_z;
-
-// 		gyro_x_out = gyro_x;
-// 		gyro_y_out = gyro_y;
-// 		gyro_z_out = gyro_z;
-// 	}
-
-// }
-// #endif
 
 int main(void)
 {
@@ -77,18 +42,6 @@ int main(void)
 		printk("Cannot set sampling frequency for gyro.\n");
 		return 0;
 	}
-
-// #ifdef CONFIG_LSM6DSR_TRIGGER
-// 	struct sensor_trigger trig;
-
-// 	trig.type = SENSOR_TRIG_DATA_READY;
-// 	trig.chan = SENSOR_CHAN_ACCEL_XYZ;
-
-// 	if (sensor_trigger_set(lsm6dsr_dev, &trig, lsm6dsr_trigger_handler) != 0) {
-// 		printk("Could not set sensor type and channel\n");
-// 		return 0;
-// 	}
-// #endif
 
 	if (sensor_sample_fetch(lsm6dsr_dev) < 0) {
 		printk("Sensor sample update error\n");
@@ -130,6 +83,6 @@ int main(void)
 		printk("loop:%d\n\n", ++cnt);
 
 		print_samples = 1;
-		k_sleep(K_MSEC(2000));
+		k_sleep(K_MSEC(1000));
 	}
 }
